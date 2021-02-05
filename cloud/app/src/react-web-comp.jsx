fe280cfa3a327c8b77f31a0abf49abcfcf6c2c32
Christian Fritz <christian.fritz@gmail.com>
2021-02-05 12:00:02 -0800

    Re #2: bootstrap + fontawesome icons
    - switched to bootstrap (semantic-ui seems dead)
    - figured out how to use font-awesome icons
      also see https://stackoverflow.com/questions/54546007/why-doesnt-font-awesome-work-in-my-shadow-dom on why it wasn't working before

--- cloud/app/src/react-web-comp.jsx
+++ cloud/app/src/react-web-comp.jsx
@@ -1,19 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import ReactWebComponent from 'react-web-component';
 import _ from 'lodash';
-// import 'semantic-ui-css/semantic.min.css';
-// const css = require('semantic-ui-css/semantic.min.css');
-import sem from 'semantic-ui-css/semantic.min.css';
-const css = sem.toString();
+import Button from 'react-bootstrap/Button';
 
-// import './test.css';
-
-import { Accordion } from 'semantic-ui-react';
-import { Button } from 'semantic-ui-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const styles = {
   wrapper: {
-    backgroundColor: 'black',
+     // backgroundColor: 'black',
+    border: '1px solid gray',
+    padding: '1em',
     textAlign: 'left',
     width: '80vw'
   },
@@ -21,43 +18,11 @@ const styles = {
     marginLeft: '1em'
   },
   fold: {
+  },
+  icon: {
+    height: '1em'
   }
 };
-
-
-
-const level1Panels = [
-  { key: 'panel-1a', title: 'Level 1A', content: 'Level 1A Contents' },
-  { key: 'panel-ba', title: 'Level 1B', content: 'Level 1B Contents' },
-]
-
-const Level1Content = (
-  <div>
-    Welcome to level 1
-    <Accordion.Accordion panels={level1Panels} />
-  </div>
-)
-
-const level2Panels = [
-  { key: 'panel-2a', title: 'Level 2A', content: 'Level 2A Contents' },
-  { key: 'panel-2b', title: 'Level 2B', content: 'Level 2B Contents' },
-]
-
-const Level2Content = (
-  <div>
-    Welcome to level 2
-    <Accordion.Accordion panels={level2Panels} />
-  </div>
-)
-
-const rootPanels = [
-  { key: 'panel-1', title: 'Level 1', content: { content: Level1Content } },
-  { key: 'panel-2', title: 'Level 2', content: { content: Level2Content } },
-]
-
-const AccordionExampleNested = () => (
-  <Accordion defaultActiveIndex={0} panels={rootPanels} styled />
-)
 
 
 
@@ -105,9 +70,7 @@ const Site = ({obj}) => <div>
 
 /** a fleet is an object of sites */
 const Fleet = ({obj}) => <div>
-  <div className="ui button">test: should be a button</div>
-  <Button>Click Here</Button>
-  <AccordionExampleNested />
+  <Button variant="primary">Primary</Button>
   {_.map(obj, (site, siteName) =>
     <div key={siteName}>{siteName}:
       <Site obj={site} />
@@ -145,7 +108,10 @@ class App extends React.Component {
   render() {
     console.log('rendering web component');
     return <div style={styles.wrapper}>
-      <style>{css}</style>
+      <style>
+        @import url("https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css");
+      </style>
+      <FontAwesomeIcon icon={faCoffee} style={styles.icon}/>
       <Diagnostics />
     </div>;
   }
@@ -153,3 +119,4 @@ class App extends React.Component {
 
 ReactWebComponent.create(<App />, 'react-web-component');
 // ReactWebComponent.create(<App />, 'react-web-component', false);
+// @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css");

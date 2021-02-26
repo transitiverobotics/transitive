c89c3a05dca81f52f46968a4de89f1d8045b77a5
Christian Fritz <christian.fritz@gmail.com>
2021-02-25 18:09:07 -0800

    re #8: wip

--- robot-agent/republish.sh
+++ robot-agent/republish.sh
@@ -6,7 +6,7 @@ else
   HOST=http://localhost:6000
 fi;
 
-if (node -e "x = `npm show --json`; y = `cat package.json`; if (x.version == y.version) process.exit(1)"); then
+if (node -e "x = `npm show --json --registry=$HOST`; y = `cat package.json`; if (x.version == y.version) process.exit(1)"); then
   npm publish --registry=$HOST
 else
   echo "no new version, not republishing"

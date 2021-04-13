808ee82fa10d23219de08b057c434fc970655985
Christian Fritz <christian.fritz@gmail.com>
2021-04-13 14:58:27 -0700

    wip: HM, aggregate on server
    - next: client side component for fleet

--- cloud/app/server_utils.js
+++ cloud/app/server_utils.js
@@ -5,4 +5,19 @@ const sharedUtils = require('./utils');
 const randomId = (bytes = 16) =>
   crypto.randomBytes(bytes).toString('base64');
 
-module.exports = Object.assign({ randomId }, sharedUtils);
+
+/** COPIED from portal utils: TODO: merge such utils somewhere */
+const parseMQTTTopic = (topic) => {
+  const parts = topic.split('/');
+  return {
+    organization: parts[1],
+    device: parts[2],
+    capability: parts[3],
+    sub: parts.slice(4)
+  }
+};
+
+module.exports = Object.assign({
+    randomId,
+    parseMQTTTopic
+  }, sharedUtils);

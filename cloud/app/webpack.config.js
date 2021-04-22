1a048763197c359feac7fb7569647b6e2fafd0e5
Christian Fritz <christian.fritz@gmail.com>
2021-04-22 16:14:42 -0700

    wip: using DataCache everywhere to facilitate data sync
    DataCache now used in
    - robot-agent
    - Capability class (not yet in HealthMonitoring capability)
    - device health web component

--- cloud/app/webpack.config.js
+++ cloud/app/webpack.config.js
@@ -17,7 +17,16 @@ module.exports = {
     rules: [{
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react'
+          ],
+          plugins: [
+            '@babel/plugin-proposal-class-properties'
+          ]
+        },
       }, {
         test: /\.css$/i,
         use: [

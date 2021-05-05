744e3b2233498ace7cec5b4acabd3cea39040d51
Christian Fritz <christian.fritz@gmail.com>
2021-05-04 17:26:33 -0700

    wip: most subitems of #20 are done (remote access cap)

---
+++ cloud/app/caps/remote_access_authorized_keys_command/main.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const MongoClient = require('mongodb').MongoClient;
+
+const URL = process.env.MONGO_URL || 'mongodb://localhost:3001';
+const DB_NAME = process.env.MONGO_DB || 'meteor';
+
+const fetchKeys = async () => {
+  const client = new MongoClient(URL, {useUnifiedTopology: true});
+  await client.connect();
+  const db = client.db(DB_NAME);
+  const deviceCollection = db.collection('devices');
+
+  await deviceCollection.find({'remote_access.publicKey': {$exists: true}},
+    {projection: {'remote_access.publicKey': true}}
+  ).forEach(device => console.log(device.remote_access.publicKey.trim()));
+
+  client.close();
+};
+
+
+if (process.argv[2] == 'tunnel') {
+  // this is only for the 'tunnel' user
+  fetchKeys();
+}

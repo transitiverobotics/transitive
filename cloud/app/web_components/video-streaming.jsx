2ab22a2558b683ceff2b04a6734e0c2e93bae0aa
Christian Fritz <christian.fritz@gmail.com>
2021-05-31 17:59:14 -0700

    wip: video-streaming, add timeout
    - also doing work on mqtt_tool, needed for debugging

--- cloud/app/web_components/video-streaming.jsx
+++ cloud/app/web_components/video-streaming.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState } from 'react';
 import ReactWebComponent from 'react-web-component';
 
 import { useDataSync } from './hooks.js';
@@ -7,8 +7,33 @@ import { InlineCode } from './shared.jsx';
 const styles = {
 };
 
+let interval;
+
+const Timer = ({duration, onTimeout, onStart}) => {
+  const [timer, setTimer] = useState(duration || 60);
+
+  if (!interval && timer > 0) {
+    interval = setInterval(() =>
+      setTimer(t => {
+        if (t > 0) {
+          return --t;
+        } else {
+          onTimeout && onTimeout();
+          clearInterval(interval);
+          interval = null;
+        }
+      }), 1000);
+    onStart && onStart();
+  }
+
+  return timer > 0 ? <div>Timeout in: {timer} seconds</div>
+  : <div>Timed out.
+    <button onClick={() => setTimer(duration)}>Restart</button>
+  </div>;
+};
 
 const Device = (props) => {
+  const [running, setRunning] = useState(false);
   // note: props must include jwt and id
   window.tr_devmode && console.log('video-stream');
 
@@ -19,7 +44,11 @@ const Device = (props) => {
   const urlParams = Object.entries(params).map(x => x.join('=')).join('&');
 
   return <div>
-    <img src={`http${TR_SECURE ? 's' : ''}://video.${TR_HOST}/stream?${urlParams}`} />
+    {running &&
+        <img src={`http${TR_SECURE ? 's' : ''}://video.${TR_HOST}/stream?${urlParams}`} />
+    }
+    <Timer onTimeout={() => setRunning(false)} duration={4}
+      onStart={() => setRunning(true)}/>
   </div>
 };
 

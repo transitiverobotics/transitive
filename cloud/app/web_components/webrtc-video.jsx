0335be7bd44ca47dd278d4d3f61517827ab76205
Christian Fritz <christian.fritz@gmail.com>
2021-07-22 11:49:43 -0700

    webrtc-video: receive ice candidates in bulk

--- cloud/app/web_components/webrtc-video.jsx
+++ cloud/app/web_components/webrtc-video.jsx
@@ -4,16 +4,14 @@ import ReactWebComponent from 'react-web-component';
 import { Button } from 'react-bootstrap';
 
 import { useDataSync } from './hooks.js';
-import { InlineCode } from './shared.jsx';
+import { Timer } from './shared.jsx';
 
 const styles = {
 };
 
 const decodeJWT = (jwt) => JSON.parse(atob(jwt.split('.')[1]));
 
-let channel;
 let connection;
-let connected = false;
 
 const sessionId = Math.random().toString(36).slice(2);
 
@@ -23,84 +21,100 @@ const Device = (props) => {
     = useDataSync({ jwt: props.jwt, id: props.id,
       publishPath: `+.+.+.${sessionId}.client` });
   const {device} = decodeJWT(props.jwt);
+  let connected = false;
   const video = useRef(null);
+
+  const startVideo = () => {
+    // request an audience (webrtc connection) with the device
+    dataCache.updateFromArray(
+      [props.id, device, 'webrtc-video', sessionId, 'client', 'request'], new Date()
+    );
+
+    dataCache.subscribePath(`+.+.+.${sessionId}.server.spec`, (serverSpec, key) => {
+
+      if (connected) {
+        console.log('already connected, sort of', connection?.connectionState,
+          connected);
+        return;
+      }
+      const {offer, candidates, turnCredentials} = JSON.parse(serverSpec);
+      console.log({offer, candidates, turnCredentials});
+
+      connection = new RTCPeerConnection({
+        iceServers: [{
+          // urls: "stun:stun.l.google.com:19302"
+          urls: "turn:localhost.localdomain:3478",
+          username: turnCredentials.username,
+          credential: turnCredentials.password,
+        }],
+        iceTransportPolicy: "all",
+      });
+
+      connection.onconnectionstatechange =
+        event => console.log(connection.connectionState, event);
+
+      connection.ontrack = (event) => {
+        // video.current.srcObject = event.streams[0];
+        video.current.srcObject = new MediaStream([event.track]);
+      };
+
+      // console.log(connection.connectionState);
+      // !connected
+      !connected && connection.setRemoteDescription(offer).then(() => {
+          console.log('set remote', connection.connectionState);
+          return Promise.all(
+            candidates.map(c => connection.addIceCandidate(c)));
+        }).then(() => {
+          console.log('create answer', connection.connectionState);
+          return connection.createAnswer();
+        }).then((answer) => {
+          // set bitrate:
+          const kbitPerSeconds = 500;
+          answer.sdp = answer.sdp.replace(/(c=.*\r\n)/,
+            `$1b=AS:${kbitPerSeconds}\r\n`);
+          if (connection.signalingState != 'stable') {
+            return connection.setLocalDescription(answer);
+          }
+        }).then(() => {
+          connected = true;
+          dataCache.updateFromArray(
+            [props.id, device, 'webrtc-video', sessionId, 'client', 'spec'],
+            JSON.stringify({
+              answer: connection.localDescription.toJSON()
+            })
+          );
+        }).catch((err) => {
+          console.log('error in establishing connection from spec', err);
+        });
+    });
+
+    return () => {
+      console.log('disconnecting');
+      connection.close();
+    };
+  };
+
 
   useEffect(() => {
       if (!ready) {
         return;
       }
 
-      // request an audience (webrtc connection) with the device
-      dataCache.updateFromArray(
-        [props.id, device, 'webrtc-video', sessionId, 'client', 'request'], new Date()
-      );
-
-      dataCache.subscribePath(`+.+.+.${sessionId}.server.spec`, (serverSpec) => {
-        if (connected) {
-          console.log('already connected, sort of');
-          return;
-        }
-        const {offer, candidate, turnCredentials} = JSON.parse(serverSpec);
-        console.log({offer, candidate, turnCredentials});
-
-        connection = new RTCPeerConnection({
-          iceServers: [{
-            // urls: "stun:stun.l.google.com:19302"
-            urls: "turn:localhost.localdomain:3478",
-            username: turnCredentials.username,
-            credential: turnCredentials.password,
-          }]
-        });
-
-        connection.onconnectionstatechange =
-          event => console.log(connection.connectionState, event);
-
-        connection.ontrack = (event) => {
-          console.log('received track', event);
-          // video.current.srcObject = event.streams[0];
-          video.current.srcObject = new MediaStream([event.track]);
-        };
-
-        !connected && connection.setRemoteDescription(offer).then(() => {
-            console.log('description set!');
-            connected = true;
-            return connection.addIceCandidate(candidate);
-          }).then(() => {
-            console.log('ice set!');
-            return connection.createAnswer();
-          }).then((answer) => {
-            // set bitrate:
-            const kbitPerSeconds = 500;
-            answer.sdp = answer.sdp.replace(/(c=.*\r\n)/,
-              `$1b=AS:${kbitPerSeconds}\r\n`);
-            console.log({answer});
-            return connection.setLocalDescription(answer);
-          }).then(() => {
-            console.log('sending answer to server');
-            dataCache.updateFromArray(
-              [props.id, device, 'webrtc-video', sessionId, 'client', 'spec'],
-              JSON.stringify({
-                answer: connection.localDescription.toJSON()
-              })
-            );
-          }).catch((err) => {
-            console.log('error in establishing connection from spec', err);
-          });
-      });
-
-      return () => {
-        console.log('disconnecting');
-        connection.close();
-      };
+      return startVideo();
     }, [ready]);
 
 
   return <div>
     <video ref={video} autoPlay muted/>
     <div>
-      <Button onClick={() => connection.close()}>
+      <Button onClick={() => video.current.pause()}>
         stop
       </Button>
+      {<Timer duration={20}
+          onTimeout={() => connection.close()}
+          onStart={() => startVideo()}
+          />
+      }
     </div>
   </div>
 };
@@ -110,6 +124,7 @@ class App extends React.Component {
   webComponentDisconnected() {
     console.log('closing webrtc connection');
     connection.close();
+
   }
 
   render() {

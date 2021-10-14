93357497b526c31706296a9e920ebcb36372e9e0
Christian Fritz <christian.fritz@gmail.com>
2021-10-14 13:40:45 -0700

    startPackage.sh: also run npm install
    - in addition to `npm update`. This ensures missing dependencies (e.g., native code that failed to compile) get installed, too

--- robot-agent/startPackage.sh
+++ robot-agent/startPackage.sh
@@ -18,6 +18,9 @@ node -e "fs.writeFileSync('password', Math.random().toString(36).substr(2, 9))"
 npm update --no-save
 # Note: npm update also installs missing packages, see,
 # https://stackoverflow.com/a/19824154/1087119
+# But we still need to run `npm install` to make sure all dependencies
+# are installed as well (e.g., if compilation of native code failed last time)
+npm install --no-save
 
 cd node_modules/@transitive-robotics/$1
 env PASSWORD=$(cat ../../../password) npm start

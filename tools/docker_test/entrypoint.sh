9fb37d698e8ac689a3dbb76045447734c862ca99
Christian Fritz <christian.fritz@gmail.com>
2022-04-22 09:09:09 -0700

    Simplified the test robot docker

--- tools/docker_test/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 172.17.0.1 ${HOST} {registry,portal,data,auth,install,repo,mqtt}.${HOST} >> /etc/hosts
-
-hostname > /etc/machine-id
-
-bash
--- tools/docker_test/run.sh
+++ tools/docker_test/run.sh
@@ -12,11 +12,14 @@ fi;
 
 docker build $BUILDARGS -t $TAGNAME .
 
+. ../../cloud/.env
 mkdir -p /tmp/transitive-docker-robot
+
 docker run -it --rm \
 --env-file ../../cloud/.env \
 --privileged \
 --hostname robot_$(date -Iseconds | tr -d ':-' | cut -c -15) \
 -v /tmp/transitive-docker-robot:/root/.transitive \
 --name robot \
+--add-host={registry,portal,data,auth,install,repo,mqtt}.${HOST}:host-gateway \
 $TAGNAME bash

5858adfb98a673a27c833f8cd10b15e0758c0abd
Christian Fritz <christian.fritz@gmail.com>
2022-07-24 15:36:03 -0700

    robot-agent: fix self-update
    - it was broken in the systemd context, because the `npm update` call would interrupt itself (by doing `systemctl restart..` in postinstall), and hence never finished, creating an infinite loop instead.
    - new setup is much simpler and reuses the same `start_agent.sh` script used in non-systemd context (e.g., docker)

--- robot-agent/start_agent.sh
+++ robot-agent/start_agent.sh
@@ -4,17 +4,26 @@ DIR=~/.transitive
 NODE=$DIR/usr/bin/node
 NPM="$NODE $DIR/usr/bin/npm"
 
+export PATH=$DIR/usr/sbin:$DIR/usr/bin:$DIR/sbin:$DIR/bin:$PATH
+
 # for n in $(cat $DIR/.env | grep -v "^#"); do export $n; done
 # if [[ -f $DIR/.env_user ]]; then
 #   for n in $(cat $DIR/.env_user | grep -v "^#"); do export $n; done
 # fi;
 
 # export PATH=$PATH:$DIR/usr/sbin:$DIR/usr/bin:$DIR/sbin:$DIR/bin
-export NO_SYSTEMD=1
+# export NO_SYSTEMD=1
 while (true); do
+  cd $DIR
+  # The `rm` here is to clear out old npm folders from a potentially failed
+  # update (or whatever else leaves these beind, see
+  # https://docs.npmjs.com/common-errors#many-enoent--enotempty-errors-in-output.
+  rm -rf node_modules/.*-* node_modules/@*/.*-*
+  $NPM update --no-save
+
+  ./generate_certs.sh
+
   cd $DIR/node_modules/@transitive-robotics/robot-agent
-  # Yes this^ needs to be in the loop, because after a self-update we need to
-  # re-enter that directory (it gets rewritten during npm update).
   $NPM start;
   sleep 2;
   echo "Restarting the agent"

ffc20b8aa5bddbe0063b387e94e826b263f8a98e
Christian Fritz <christian.fritz@gmail.com>
2023-01-25 20:49:32 -0800

    Homepage: add meta data for social previews to cap pages
    - also added social media icons to homepage (via CSS)
    
    Fixes #220.

--- cloud/mongo/init.js
+++ cloud/mongo/init.js
@@ -2,5 +2,10 @@
 rs.initiate({
   _id: 'rs0',
   version: 1,
-  members: [{_id: 0, host: '127.0.0.1:27017'}]
+  members: [{
+    _id: 0,
+    host: 'mongodb:27017'
+    // Apparently this needs to be a name known to all clients, i.e., can't
+    // be 127.0.0.1. Otherwise clients must use `directConnection`.
+  }]
 })

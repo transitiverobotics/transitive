b1c6a7fcf528ae89697918518a95483201ec40ce
Christian Fritz <christian.fritz@gmail.com>
2023-08-31 11:26:41 -0700

    install.sh: download nodejs debs from our public s3 bucket

--- cloud/app/assets/install.sh
+++ cloud/app/assets/install.sh
@@ -17,7 +17,9 @@ NORMAL="\033[0;39m"
 ARCH=$(dpkg --print-architecture)
 # NODEURL=https://deb.nodesource.com/node_16.x/pool/main/n/nodejs/nodejs_16.14.2-deb-1nodesource1_${ARCH}.deb
 # no longer available! now self hosting:
-NODEURL=https://transitiverobotics.com/static/nodejs_16.20.2-1nodesource1_${ARCH}.deb
+# NODEURL=https://transitiverobotics.com/static/nodejs_16.20.2-1nodesource1_${ARCH}.deb
+#
+NODEURL=https://public-package-repo.s3.us-west-2.amazonaws.com/nodejs_16.20.2-1nodesource1_${ARCH}.deb
 NODEDEB=/tmp/transitive_nodejs16.deb
 DIR=~/.transitive
 NODE=$DIR/usr/bin/node

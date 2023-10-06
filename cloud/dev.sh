7bca7bd87ab3821ba5a83b1e1274014bc2d2acbe
Christian Fritz <christian.fritz@gmail.com>
2023-10-06 13:25:27 -0700

    Added a new service, mDNS, for announcing subdomains in dev

--- cloud/dev.sh
+++ cloud/dev.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # "source" .env
-export $(cat .env | grep -v ^\# | xargs)
+set -o allexport
+. .env
+set +o allexport
 
 # We need to create the certs folder to make sure the generated certs have the
 # right owner (see certs/generate.sh).
-mkdir -p TR_VAR_DIR/certs
+mkdir -p $TR_VAR_DIR/certs
 
 # Enable lines commented out with #DEV and remove those marked #NODEV.
 # This allows us to use slightly different setups in dev than in prod.

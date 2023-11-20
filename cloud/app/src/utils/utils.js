9b990b89aaeaf185ed8184f044e49e9154e5dc54
Christian Fritz <christian.fritz@gmail.com>
2023-11-20 15:42:25 -0800

    app: support runtime-defined cap widgets
    - this makes it unnecessary to first publish the capability to the registry and also allows defining multiple widgets in the same js bundle

--- cloud/app/src/utils/utils.js
+++ cloud/app/src/utils/utils.js
@@ -1,8 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 /** ensure the named web component is loaded; if not, it is loaded assuming
   the .js file it is defined in has the same name as the component itself */
 export const ensureWebComponentIsLoaded = (capability, name, userId, deviceId) => {
+  const [ready, setReady] = useState(false);
+
   useEffect(() => {
       if (userId && deviceId && !customElements.get(name)) {
         const url = new URL(location.href);
@@ -10,7 +12,15 @@ export const ensureWebComponentIsLoaded = (capability, name, userId, deviceId) =
         const params = `userId=${userId}&deviceId=${deviceId}`;
         script.setAttribute('src', `/running/${capability}/dist/${name}.js?${params}`);
         // `${url.protocol}//data.${url.host}/bundle/${capability}/dist/${name}.js?${params}`);
+        script.onload = () => {
+          console.log(`loaded ${name}`);
+          setReady(true);
+        };
         document.head.appendChild(script);
+      } else {
+        setReady(true);
       }
     }, [capability, name, userId, deviceId]);
+
+  return {ready};
 };

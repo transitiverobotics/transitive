d8f774cfc989cd48364d67c800dd96115ade4a0b
Christian Fritz <christian.fritz@gmail.com>
2024-01-11 17:25:01 -0800

    mDNS: need to mount entire /etc folder to create files

--- cloud/tools/mDNS/enable_mdns.sh
+++ cloud/tools/mDNS/enable_mdns.sh
@@ -3,16 +3,16 @@
 # https://github.com/transitiverobotics/transitive/blob/main/cloud/tools/mDNS/README.md
 
 # Ensure local subdomains are enabled
-cp /nsswitch.conf /tmp/nsswitch.conf
+cp /host_etc/nsswitch.conf /tmp/nsswitch.conf
 # Note that we can't use sed -i inside a docker on a mounted file:
 # https://unix.stackexchange.com/a/404356/53593
 sed -i 's/mdns4_minimal/mdns4/' /tmp/nsswitch.conf
-cp /tmp/nsswitch.conf /nsswitch.conf
+cp /tmp/nsswitch.conf /host_etc/nsswitch.conf
 
 # Ensure .local domains are allowed
-if ( ! grep '\.local\.$' /mdns.allow ); then
-  echo '.local.' >> /etc/mdns.allow
+if ( ! grep '\.local\.$' /host_etc/mdns.allow ); then
+  echo '.local.' >> /host_etc/mdns.allow
 fi
-if ( ! grep '\.local$' /mdns.allow ); then
-  echo '.local' >> /etc/mdns.allow
+if ( ! grep '\.local$' /host_etc/mdns.allow ); then
+  echo '.local' >> /host_etc/mdns.allow
 fi

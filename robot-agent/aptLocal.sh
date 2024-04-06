51881620c609dd53b3042b0ae189326abe059bb7
Christian Fritz <christian.fritz@gmail.com>
2024-04-06 13:05:37 -0700

    aptLocal: fix bug in ubuntu 22: no ROS one repo
    Fixes #478

--- robot-agent/aptLocal.sh
+++ robot-agent/aptLocal.sh
@@ -19,14 +19,17 @@ printStep() {
 getROSRelease() {
   case $(lsb_release -sc) in
     xenial) echo kinetic;;
-    focal) echo noetic;;
     bionic) echo melodic;;
-    *) echo melodic;;
+    focal) echo noetic;;
   esac
 }
 
 BASEDIR=$(dirname $0)
 DIR=~/.transitive
+
+# ROS_RELEASE=$(ls -1 --color=never ~/.transitive/opt/ros | head -n 1)
+# ^^ would fail if no ros packages were yet installed
+ROS_RELEASE=$(getROSRelease)
 
 # -------------------------------------------------------------------------
 
@@ -56,7 +59,13 @@ cat $(ls $DIR/var/lib/dpkg/status.d/.merged/* | xargs) > $DIR/var/lib/dpkg/statu
 printStep "Set apt sources"
 # ROS
 cp {,$DIR}/etc/apt/sources.list
-echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > $DIR/etc/apt/sources.list.d/ros-latest.list
+if [[ -z $ROS_RELEASE ]]; then
+  echo "No ROS1 release for $(lsb_release -sc)";
+  # remove it in case an older version of aptLocal added it:
+  rm -rf $DIR/etc/apt/sources.list.d/ros-latest.list
+else
+  echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > $DIR/etc/apt/sources.list.d/ros-latest.list
+fi;
 
 if [[ -e /etc/apt/trusted.gpg ]]; then cp {,$DIR}/etc/apt/trusted.gpg; fi
 cp /etc/apt/trusted.gpg.d/* $DIR/etc/apt/trusted.gpg.d || true
@@ -66,8 +75,9 @@ printStep "Import ROS repo keys"
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | unshare -rm apt-key --keyring $DIR/etc/apt/trusted.gpg.d/ros.gpg add -
 
 # node.js:
-echo "deb [arch=amd64,arm64 signed-by=$DIR/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x nodistro main" > $DIR/etc/apt/sources.list.d/nodesource.list
-echo "deb [arch=amd64,arm64 signed-by=$DIR/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >> $DIR/etc/apt/sources.list.d/nodesource.list
+ARCH=$(dpkg --print-architecture)
+echo "deb [arch=$ARCH signed-by=$DIR/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x nodistro main" > $DIR/etc/apt/sources.list.d/nodesource.list
+echo "deb [arch=$ARCH signed-by=$DIR/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >> $DIR/etc/apt/sources.list.d/nodesource.list
 rm -f $DIR/etc/apt/keyrings/nodesource.gpg
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o $DIR/etc/apt/keyrings/nodesource.gpg
 
@@ -129,18 +139,16 @@ done
 
 # Generate env file for using these locally installed packages
 
-# ROS_RELEASE=$(ls -1 --color=never ~/.transitive/opt/ros | head -n 1)
-# ^^ would fail if no ros packages were yet installed
-ROS_RELEASE=$(getROSRelease)
 cat > $DIR/etc/env_local << EOF
 # environment variables for using debian packages installed via aptLocal.sh
 # i.e., locally in ~/.transitive
 
-export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$DIR/lib/$(uname -m)-linux-gnu:$DIR/usr/lib/$(uname -m)-linux-gnu/:$DIR/usr/lib/:$DIR/opt/ros/$ROS_RELEASE/lib/
+# export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$DIR/lib/$(uname -m)-linux-gnu:$DIR/usr/lib/$(uname -m)-linux-gnu/:$DIR/usr/lib/:$DIR/opt/ros/$ROS_RELEASE/lib/
+export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$DIR/lib/$(uname -m)-linux-gnu:$DIR/usr/lib/$(uname -m)-linux-gnu/:$DIR/usr/lib/
 
-export ROS_PACKAGE_PATH=\$ROS_PACKAGE_PATH:$DIR/opt/ros/$ROS_RELEASE/share
+# export ROS_PACKAGE_PATH=\$ROS_PACKAGE_PATH:$DIR/opt/ros/$ROS_RELEASE/share
 
-export CMAKE_PREFIX_PATH=\$CMAKE_PREFIX_PATH:$DIR/opt/ros/$ROS_RELEASE
+# export CMAKE_PREFIX_PATH=\$CMAKE_PREFIX_PATH:$DIR/opt/ros/$ROS_RELEASE
 
 export PYTHONPATH=\$PYTHONPATH:$DIR/usr/lib/python2.7/dist-packages:$DIR/usr/lib/python3/dist-packages
 

64cf068bccbc6ab226a0e98a68c60006dafd577f
Javier Lurgo <jlurgo@gmail.com>
2025-05-13 15:09:34 -0300

    using fs.watch for config.json, rewatching on every change

--- robot-agent/config.js
+++ robot-agent/config.js
@@ -23,12 +23,45 @@ const refreshGlobalConfigFromFile = () => {
 };
 refreshGlobalConfigFromFile();
 
-fs.watchFile('./config.json', { interval: 500 }, (curr, prev) => {
-  if (curr.mtime !== prev.mtime) {
-    console.log('config.json changed, updating global config');
-    refreshGlobalConfigFromFile();
+let watcher;
+
+// Wait for the file to exist before using it
+// This is useful for VI or other editors that creates a new file
+// on save, which can cause the watcher to trigger before the file is ready
+// to be read.
+const waitForFile = (filePath, callback) => {
+  const interval = setInterval(() => {
+    if (fs.existsSync(filePath)) {
+      clearInterval(interval);
+      callback();
+    }
+  }, 500);
+};
+
+const startWatchingConfig = () => {
+  if (watcher) {
+    watcher.close();
+    console.log('Previous watcher closed.');
   }
-});
+
+  waitForFile('./config.json', () => {
+    try {
+      watcher = fs.watch('./config.json', { persistence: false }, (eventType, filename) => {
+        console.log('config.json changed');
+        waitForFile('./config.json', () => {
+          refreshGlobalConfigFromFile();
+        });
+        startWatchingConfig(); // Restart the watcher
+      });
+
+      console.log('Started watching config.json');
+    } catch (err) {
+      console.error('Error setting up watcher:', err);
+    }
+  });
+};
+
+startWatchingConfig();
 
 /** Set the `key` in the fleet config to `value` */
 const updateFleetConfig = (key, value) => fleetConfig[key] = value;

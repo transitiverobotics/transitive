2eb7a883959face0d8c47f3696bd2510385ab628
Christian Fritz <christian.fritz@gmail.com>
2025-06-16 13:16:36 -0700

    Logging follow-up:
    - separate hyperDX services
    - fixed HYPERDX_API_KEY logic (reading it from DB in portal)
    - cleaned up portal server code, sending log line on portal start, mostly for testing
    - added hyperDX to proxy
    - robot-agent:
      - cleaned up LogMonitor start logic (was distributed over two files)
      - added doc-comments in some places I was able to
    
    Note: in this commit logging is not currently working -- somehow the distributed hyperdx services are not working as expected (cannot end logs to otel-collector, which is difficult to debug).
    
    fixed non-working otel-collector
    
    - it was just a bad env var
    - small but required fixes re. urls
    
    clickhouse: limit memory in docker-compose
    
    remove mem limit for clickhouse to avoid restarts

--- robot-agent/logMonitor.js
+++ robot-agent/logMonitor.js
@@ -6,8 +6,8 @@ const { getLogger, topicToPath} = require('@transitive-sdk/utils');
 const log = getLogger('logMonitor.js');
 log.setLevel('info');
 
-
-const getLogLevel = (levelName) => {
+/** Translate log level into a numeric value. */
+const getLogLevelValue = (levelName) => {
   const levels = {
     'DEBUG': 10,
     'INFO': 20,
@@ -19,18 +19,20 @@ const getLogLevel = (levelName) => {
   return levels[levelName.toUpperCase()] || 20; // default to INFO if not found
 };
 
+/** Get the currently configured level of logging we want to forward from the
+ * named package (or robot-agent) */
 const getMinLogLevel = (packageName) => {
   const globalMinLogLevel = _.get(global.config, 'global.minLogLevel', 'ERROR');
-  let minLogLevel;
-  if (packageName === 'robot-agent') {
-    minLogLevel = globalMinLogLevel;
-  } else {
-    minLogLevel = _.get(global.config, `${packageName}.minLogLevel`, globalMinLogLevel);
-  }
-  return minLogLevel;
+  return (packageName === 'robot-agent' ? globalMinLogLevel
+    : _.get(global.config, `${packageName}.minLogLevel`, globalMinLogLevel)
+  );
 };
 
+/** The log monitor accepts packages to monitor, watches their logs and
+* publishes them (filtered) to the cloud via MQTT (not sync). ALso does this
+* for the agent log. */
 class LogMonitor {
+
   constructor() {
     this.mqttClient = null;
     this.mqttSync = null;
@@ -39,6 +41,8 @@ class LogMonitor {
     this.pendingLogs = []; // Array to store pending logs
     this.initialized = false;
   }
+
+
   init(mqttClient, mqttSync, agentPrefix){
     this.mqttClient = mqttClient;
     this.mqttSync = mqttSync;
@@ -48,18 +52,21 @@ class LogMonitor {
       // Update minLogLevel for all watched packages
       Object.keys(this.watchedPackages).forEach(packageName => {
         const minLogLevel = getMinLogLevel(packageName);
-        if (this.watchedPackages[packageName].minLogLevel === minLogLevel) return; // No change in minLogLevel
+        if (this.watchedPackages[packageName].minLogLevel === minLogLevel)
+          return; // No change in minLogLevel
         log.info('Updating minLogLevel for', packageName, 'from',
           this.watchedPackages[packageName].minLogLevel,
           'to', minLogLevel
         );
         this.watchedPackages[packageName].minLogLevel = minLogLevel;
-        this.watchedPackages[packageName].minLogLevelValue = getLogLevel(minLogLevel);
+        this.watchedPackages[packageName].minLogLevelValue = getLogLevelValue(minLogLevel);
       });
     });
     this.initialized = true; // Set initialized state
     this.processNextPendingLog(); // Start processing pending logs
   }
+
+
   watchLogs(packageName) {
     if (_.get(this.watchedPackages, packageName)) {
       log.warn('Package is already being watched:', packageName);
@@ -68,7 +75,7 @@ class LogMonitor {
     let filePath = '';
     let topicSuffix = '';
     let lastLogTimestampFilePath = '';
-    
+
     if (packageName === 'robot-agent') {
       log.info('Watching logs for robot-agent package');
       filePath = `${process.env.HOME}/.transitive/agent.log`;
@@ -77,12 +84,13 @@ class LogMonitor {
     } else {
       log.info('Watching logs for package:', packageName);
       filePath = `${process.env.HOME}/.transitive/packages/${packageName}/log`;
-      lastLogTimestampFilePath = `${process.env.HOME}/.transitive/packages/${packageName}/lastTimestamp`;
-      topicSuffix = `/capabilities/${packageName}`;     
+      lastLogTimestampFilePath =
+        `${process.env.HOME}/.transitive/packages/${packageName}/lastTimestamp`;
+      topicSuffix = `/capabilities/${packageName}`;
     }
-    
+
     let minLogLevel = getMinLogLevel(packageName);
-    let minLogLevelValue = getLogLevel(minLogLevel);
+    let minLogLevelValue = getLogLevelValue(minLogLevel);
     this.watchedPackages[packageName] = {
       filePath,
       lastLogTimestampFilePath,
@@ -90,16 +98,17 @@ class LogMonitor {
       minLogLevel,
       minLogLevelValue,
     };
-    log.debug('WatchingLogs with', { filePath, topicSuffix, minLogLevel, minLogLevelValue });
+    log.debug('WatchingLogs with', { filePath, topicSuffix, minLogLevel,
+      minLogLevelValue });
 
     // first upload all log lines newer than the last log sent
     const lastLogTimestamp = this.getLastLogTimestamp(packageName);
-    
+
     const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' });
     const lines = fileContent.split('\n');
     for (const line of lines) {
       const logObject = this.parseLogLine(line, packageName);
-      if (!logObject) continue; // skip lines that are not valid log lines      
+      if (!logObject) continue; // skip lines that are not valid log lines
       const { timestamp } = logObject;
       if (!timestamp) {
         log.warn('Skipping log line without timestamp:', line);
@@ -112,7 +121,7 @@ class LogMonitor {
       }
       this.pendingLogs.push({ logObject, packageName });
     }
-    
+
     const tail = new Tail(filePath);
 
     tail.on('line', async (line) => {
@@ -123,16 +132,20 @@ class LogMonitor {
 
     tail.on('error', (error) => {
       log.error('Error tailing log file:', filePath, error.message);
-      this.watchedPackages[packageName] = null; // Remove from watched packages on error
+      // Remove from watched packages on error
+      this.watchedPackages[packageName] = null;
     });
 
     tail.on('close', () => {
       log.info('Stopped tailing log file:', filePath);
-      this.watchedPackages[packageName] = null; // Remove from watched packages on close
+      // Remove from watched packages on close
+      this.watchedPackages[packageName] = null;
     });
 
     log.info('Started tailing log file:', filePath);
   }
+
+
   getLastLogTimestamp(packageName) {
     const filePath = this.watchedPackages[packageName].lastLogTimestampFilePath;
     let lastLogTimestamp = 0;
@@ -150,70 +163,99 @@ class LogMonitor {
     log.info('Last log timestamp for', packageName, 'is', lastLogContent);
     return lastLogTimestamp;
   }
+
+
   parseLogLine(line, packageName) {
     if (!line.startsWith('[')) return;
+
     // Remove the leading '[' and split by spaces
     const endBracketIdx = line.indexOf(']');
     if (endBracketIdx === -1) return null;
+
     const header = line.slice(1, endBracketIdx);
     const message = line.slice(endBracketIdx + 2); // skip "] "
-    if (!header || !message) return null; // Ensure both header and message are present
+
+    // Ensure both header and message are present
+    if (!header || !message) return null;
+
     const parts = header.split(' ');
-    if (parts.length < 3) return null; // Ensure there are at least 3 parts (timestamp, module, level)
-    const [timestamp, module, level] = parts;
+    // Ensure there are at least 3 parts (timestamp, module, level)
+    if (parts.length < 3) return null;
+
+    const [timestamp, moduleName, level] = parts;
     // Ignore logs produced by this module
-    if (module === 'logMonitor.js') return null;
-    if (!timestamp || !module || !level) return null; // Ensure all parts are present
-    const logLevelValue = getLogLevel(level);
+
+    if (moduleName === log.name) return null;
+
+    // Ensure all parts are present
+    if (!timestamp || !moduleName || !level) return null;
+
+    const logLevelValue = getLogLevelValue(level);
     const minLogLevelValue = this.watchedPackages[packageName].minLogLevelValue;
-    if (logLevelValue < minLogLevelValue) return null; // Skip logs below the minimum log level
-    const logObject = { timestamp, module, level, logLevelValue, message };
+
+    // Skip logs below the minimum log level
+    if (logLevelValue < minLogLevelValue) return null;
+    const logObject = { timestamp, module: moduleName,
+      level, logLevelValue, message };
     return logObject;
-  } 
-  processNextPendingLog(){
+  }
+
+
+  processNextPendingLog() {
+
     if (this.pendingLogs.length === 0) {
       log.debug('No pending logs to process');
       setTimeout(() => this.processNextPendingLog(), 1000);
       return;
     }
+
     if (!this.initialized) {
       log.debug('LogMonitor not initialized yet, waiting...');
       setTimeout(() => this.processNextPendingLog(), 1000);
       return;
     }
+
     const pendingLog = this.pendingLogs.shift(); // Get the first pending log
     const {logObject, packageName} = pendingLog;
+
     if (_.get(this.watchedPackages, packageName) === null) {
-      log.warn('Package is no longer being watched:', packageName, 'Log will not be processed:', logObject);
+      log.warn('Package is no longer being watched:', packageName,
+        'Log will not be processed:', logObject);
       setTimeout(() => this.processNextPendingLog(), 100);
       return;
     }
+
     try {
-      this.publishLogAsJson(logObject, packageName)
-        .then(() => {
-          // store last log sent timestamp in a file next to the log file
-          const lastLogTimestampFilePath = this.watchedPackages[packageName].lastLogTimestampFilePath;
-          try {
-            fs.writeFileSync(lastLogTimestampFilePath, logObject.timestamp, { encoding: 'utf8' });
-          } catch (writeErr) {
-            log.error('Failed to write last log timestamp file:', lastLogTimestampFilePath, 'Error:', writeErr.message);
-          }
-          // Process the next pending log after a short delay
-          setTimeout(() => this.processNextPendingLog(), 100);
-        })
-        .catch((e) => {
+      this.publishLogAsJson(logObject, packageName).then(() => {
+        // store last log sent timestamp in a file next to the log file
+        const lastLogTimestampFilePath =
+          this.watchedPackages[packageName].lastLogTimestampFilePath;
+        try {
+          fs.writeFileSync(lastLogTimestampFilePath, logObject.timestamp,
+            { encoding: 'utf8' });
+        } catch (writeErr) {
+          log.error('Failed to write last log timestamp file:',
+            lastLogTimestampFilePath, 'Error:', writeErr.message);
+        }
+        // Process the next pending log after a short delay
+        setTimeout(() => this.processNextPendingLog(), 100);
+      }).catch((e) => {
           log.error('Failed to publish log:', logObject, 'Error:', e.message);
           // If an error occurs, we want to retry processing this log after a delay
-          this.pendingLogs.unshift(pendingLog); // Re-add the log to the front of the queue
+          // Re-add the log to the front of the queue:
+          this.pendingLogs.unshift(pendingLog);
           setTimeout(() => this.processNextPendingLog(), 1000);
-        })
+        });
     } catch (err) {
       log.error('Failed to publish log:', logObject, 'Error:', err.message);
       // If an error occurs, we want to retry processing this log after a delay
-      this.pendingLogs.unshift(pendingLog); // Re-add the log to the front of the queue
+      // Re-add the log to the front of the queue:
+      this.pendingLogs.unshift(pendingLog);
       setTimeout(() => this.processNextPendingLog(), 1000);
     }
   }
+
+
   async publishLogAsJson(logObject, packageName){
     const topicSuffix = this.watchedPackages[packageName].topicSuffix;
     const logTopic = `${this.AGENT_PREFIX}/logs${topicSuffix}`;
@@ -221,15 +263,18 @@ class LogMonitor {
     return new Promise((resolve, reject) => {
       this.mqttClient.publish(logTopic, logJson, { qos: 2 }, (err) => {
         if (err) {
-          log.error('Failed to publish log to MQTT on topic', logTopic, ':', err.message);
+          log.error('Failed to publish log to MQTT on topic', logTopic, ':',
+            err.message);
           reject(err);
         } else {
           resolve();
         }
       });
     });
-  }  
+  }
 }
+
+// Create singleton instance and export it
 const logMonitor = new LogMonitor();
 
 module.exports = logMonitor;

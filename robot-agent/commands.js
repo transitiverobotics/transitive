f815126a71fd51f13699d23000e3bed57bd74d83
Javier Lurgo <jlurgo@gmail.com>
2025-06-25 14:37:55 -0300

    show errors on the frontend
    - uuid keys for error logs into mqttSync
    - spinner during logs dismissal
    
    shorter log keys, clearing errors by iterating on mqttSync saved logs
    
    Only storing error count on mqttsync
    
    Logs view with live logs subscription
    
    last error on counter tooltip
    
    removed extra styling
    
    back to rotate logs on startup
    
    removed stderr from log view
    
    included error count and last error retrieval logic in the GetLogButtonWithCounter component
    
    packer all logs retrieval and counting functionality in the GetLogButtonWithCounter component
    
    using /status/logs as base topic + addressed review

--- robot-agent/commands.js
+++ robot-agent/commands.js
@@ -46,11 +46,14 @@ const commands = {
 
   getPkgLog: ({pkg}) => {
     return new Promise((resolve, reject) => {
-      exec(`cat ~/.transitive/packages/${pkg}/log | tail -n 100000`,
+      const logPath = (pkg === 'robot-agent') ?
+        '~/.transitive/agent.log' :
+        `~/.transitive/packages/${pkg}/log`;
+
+      exec(`cat ${logPath} | tail -n 100000`,
         (err, stdout, stderr) => resolve({
           err,
           stdout: zlib.gzipSync(stdout).toString('base64'),
-          stderr: zlib.gzipSync(stderr).toString('base64')
         }));
     });
   },

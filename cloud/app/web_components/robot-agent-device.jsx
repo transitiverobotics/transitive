75e5efa077e17e6e267a6940352ee06a12b02842
Javier Lurgo <jlurgo@gmail.com>
2025-08-08 10:17:08 -0300

    replaced <> with <F>

--- cloud/app/web_components/robot-agent-device.jsx
+++ cloud/app/web_components/robot-agent-device.jsx
@@ -485,7 +485,7 @@ const Device = (props) => {
         />
 
       <Fold title="Configuration & status">
-          <>
+          <F>
             <div style={styles.row}>
               {latestVersionData?.info?.config &&
                 <ConfigEditor info={latestVersionData.info}
@@ -500,7 +500,7 @@ const Device = (props) => {
                 packageName="robot-agent"
               />
             </div>
-          </>
+          </F>
       </Fold>
     </div>
 

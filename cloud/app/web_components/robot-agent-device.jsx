063c510a98b73f710d2da73dfceae31cb0960da3
Javier Lurgo <jlurgo@gmail.com>
2025-08-08 10:22:36 -0300

    resource metrics receives metrics directly

--- cloud/app/web_components/robot-agent-device.jsx
+++ cloud/app/web_components/robot-agent-device.jsx
@@ -245,8 +245,7 @@ const Capability = (props) => {
         <Col sm='3' style={styles.rowItem}>
           {running && !inactive && (
             <ResourceMetrics 
-              deviceData={deviceData}
-              packageName={name}
+              metrics={deviceData?.status?.metrics?.[name] || {}}
             />
           )}
         </Col>
@@ -490,8 +489,7 @@ const Device = (props) => {
             <div style={styles.row}>
               <strong>Agent Resource Usage</strong>
               <ResourceMetrics 
-                deviceData={latestVersionData}
-                packageName="robot-agent"
+                metrics={latestVersionData?.status?.metrics?.['robot-agent'] || {}}
               />
             </div>
           </F>

d418ca5017ea6355dfe35e41765d9ab196b22ed1
Christian Fritz <christian.fritz@gmail.com>
2025-08-17 16:43:32 -0700

    portal: use utils-web@14 (incl. upgrade to react 18)
    - this also makes sure the portal UI itself uses the back-off logic when connecting to mqtt fails and checks JWT validity before even trying

--- cloud/app/src/utils/Delayed.jsx
+++ cloud/app/src/utils/Delayed.jsx
@@ -3,7 +3,9 @@ import React, { useState, useEffect, useContext } from 'react';
 export const Delayed = ({delay = 500, children}) => {
 
   const [show, setShow] = useState(false);
-  useEffect(() => setTimeout(() => setShow(true), delay), []);
+  useEffect(() => {
+      setTimeout(() => setShow(true), delay);
+    }, []);
 
   const style = {
     transition: 'opacity 1s ease, maxHeight 1s linear',

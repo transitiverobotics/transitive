ed6fc3106a3adfad14f06a36758c5b5fd644d19a
Javier Lurgo <jlurgo@gmail.com>
2025-08-20 15:47:26 -0300

    create logs and metrics tables

--- cloud/app/server/telemetry.js
+++ cloud/app/server/telemetry.js
@@ -43,6 +43,99 @@ class TelemetryService {
       throw error;
     }
   }
+  async init() {
+    log.info('Initializing TelemetryService');
+    // create logs and metrics tables if they do not exist
+    try {
+      log.info('Creating logs table if it does not exist');
+      await this.clickHouseClient.command({
+        query: `
+          CREATE TABLE IF NOT EXISTS logs (
+            Timestamp DateTime64(9) CODEC(Delta(8), ZSTD(1)),
+            TimestampTime DateTime DEFAULT toDateTime(Timestamp),
+            TraceId String CODEC(ZSTD(1)),
+            SpanId String CODEC(ZSTD(1)),
+            TraceFlags UInt8,
+            SeverityText LowCardinality(String) CODEC(ZSTD(1)),
+            SeverityNumber UInt8,
+            ServiceName LowCardinality(String) CODEC(ZSTD(1)),
+            Body String CODEC(ZSTD(1)),
+            ResourceSchemaUrl LowCardinality(String) CODEC(ZSTD(1)),
+            ResourceAttributes Map(LowCardinality(String), String) CODEC(ZSTD(1)),
+            ScopeSchemaUrl LowCardinality(String) CODEC(ZSTD(1)),
+            ScopeName String CODEC(ZSTD(1)),
+            ScopeVersion LowCardinality(String) CODEC(ZSTD(1)),
+            ScopeAttributes Map(LowCardinality(String), String) CODEC(ZSTD(1)),
+            LogAttributes Map(LowCardinality(String), String) CODEC(ZSTD(1)),
+            INDEX idx_trace_id TraceId TYPE bloom_filter(0.001) GRANULARITY 1,
+            INDEX idx_res_attr_key mapKeys(ResourceAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+            INDEX idx_res_attr_value mapValues(ResourceAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+            INDEX idx_scope_attr_key mapKeys(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+            INDEX idx_scope_attr_value mapValues(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+            INDEX idx_log_attr_key mapKeys(LogAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+            INDEX idx_log_attr_value mapValues(LogAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+            INDEX idx_body Body TYPE tokenbf_v1(32768, 3, 0) GRANULARITY 8
+          )
+          ENGINE = MergeTree
+          PARTITION BY toDate(TimestampTime)
+          PRIMARY KEY (ServiceName, TimestampTime)
+          ORDER BY (ServiceName, TimestampTime, Timestamp)
+          TTL TimestampTime + toIntervalDay(3)
+          SETTINGS index_granularity = 8192, ttl_only_drop_parts = 1
+        `,
+        clickhouse_settings: {
+          wait_end_of_query: 1,
+        }
+      });
+      
+      await this.clickHouseClient.command({
+        query:`
+          CREATE TABLE IF NOT EXISTS default.metrics
+          (
+            \`ResourceAttributes\` Map(LowCardinality(String), String) CODEC(ZSTD(1)),
+            \`ResourceSchemaUrl\` String CODEC(ZSTD(1)),
+            \`ScopeName\` String CODEC(ZSTD(1)),
+            \`ScopeVersion\` String CODEC(ZSTD(1)),
+            \`ScopeAttributes\` Map(LowCardinality(String), String) CODEC(ZSTD(1)),
+            \`ScopeDroppedAttrCount\` UInt32 CODEC(ZSTD(1)),
+            \`ScopeSchemaUrl\` String CODEC(ZSTD(1)),
+            \`ServiceName\` LowCardinality(String) CODEC(ZSTD(1)),
+            \`MetricName\` String CODEC(ZSTD(1)),
+            \`MetricDescription\` String CODEC(ZSTD(1)),
+            \`MetricUnit\` String CODEC(ZSTD(1)),
+            \`Attributes\` Map(LowCardinality(String), String) CODEC(ZSTD(1)),
+            \`StartTimeUnix\` DateTime64(9) CODEC(Delta(8), ZSTD(1)),
+            \`TimeUnix\` DateTime64(9) CODEC(Delta(8), ZSTD(1)),
+            \`Value\` Float64 CODEC(ZSTD(1)),
+            \`Flags\` UInt32 CODEC(ZSTD(1)),
+            \`Exemplars.FilteredAttributes\` Array(Map(LowCardinality(String), String)) CODEC(ZSTD(1)),
+            \`Exemplars.TimeUnix\` Array(DateTime64(9)) CODEC(ZSTD(1)),
+            \`Exemplars.Value\` Array(Float64) CODEC(ZSTD(1)),
+            \`Exemplars.SpanId\` Array(String) CODEC(ZSTD(1)),
+            \`Exemplars.TraceId\` Array(String) CODEC(ZSTD(1)),
+            INDEX idx_res_attr_key mapKeys(ResourceAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+            INDEX idx_res_attr_value mapValues(ResourceAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+            INDEX idx_scope_attr_key mapKeys(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+            INDEX idx_scope_attr_value mapValues(ScopeAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+            INDEX idx_attr_key mapKeys(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+            INDEX idx_attr_value mapValues(Attributes) TYPE bloom_filter(0.01) GRANULARITY 1
+          )
+          ENGINE = MergeTree
+          PARTITION BY toDate(TimeUnix)
+          ORDER BY (ServiceName, MetricName, Attributes, toUnixTimestamp64Nano(TimeUnix))
+          TTL toDateTime(TimeUnix) + toIntervalDay(3)
+          SETTINGS index_granularity = 8192, ttl_only_drop_parts = 1
+        `,
+        clickhouse_settings: {
+          wait_end_of_query: 1,
+        }
+      });      
+      log.info('Telemetry tables created or already exist');
+    } catch (error) {
+      log.error('Failed to create telemetry tables:', error);
+      throw error;
+    }
+  }
   sendLogs = async (logs, resourceAttributes = {}) => {
     if (!Array.isArray(logs)) {
       logs = [logs];
@@ -85,7 +178,7 @@ class TelemetryService {
 
       // Use ClickHouse client to insert logs
       this.clickHouseClient?.insert({
-        table: 'otel_logs',
+        table: 'logs',
         values: clickhouseLogs,
         format: 'JSONEachRow',
       }).then(() => {
@@ -169,7 +262,7 @@ class TelemetryService {
 
     // Use ClickHouse client to insert metrics
     this.clickHouseClient?.insert({
-      table: 'otel_metrics_gauge',
+      table: 'metrics',
       values: allMetrics,
       format: 'JSONEachRow',
     }).then(() => {

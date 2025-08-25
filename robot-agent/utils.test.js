bcb336b2a645a78155b9ea2f8ad50a1d992786a3
Christian Fritz <christian.fritz@gmail.com>
2025-08-24 21:16:29 -0700

    further simplified resource monitoring and display

--- robot-agent/utils.test.js
+++ robot-agent/utils.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { getInstalledPackages } = require('./utils');
+const { getInstalledPackages, toPrecision } = require('./utils');
 
 describe('utils', function() {
   // currently only used for its output (manually inspected test)
@@ -7,4 +7,16 @@ describe('utils', function() {
     const list = getInstalledPackages();
     console.log(list);
   });
+
+  describe('toPrecision', function() {
+    it('should be correct', function() {
+      assert.equal( toPrecision(3.1234, 0), 3);
+      assert.equal( toPrecision(3.1234, 1), 3.1);
+      assert.equal( toPrecision(3.1234, 2), 3.12);
+      assert.equal( toPrecision(3.1234, 6), 3.1234);
+
+      assert.equal( toPrecision(312.34, -1), 310);
+    });
+  });
+
 });

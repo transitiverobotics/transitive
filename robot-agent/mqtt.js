f1a9cb91d19bcbe09b631af0c17a46c4a8d69fc6
Christian Fritz <christian.fritz@gmail.com>
2025-10-11 16:33:13 -0700

    robot-agent: new config option: aggressiveReconnect
    - when set to `true`, it will aggressively (force-)end the mqtt
      connection and reconnect whenever no $SYS heartbeat has been received
    for more than a threshold (currently 65 seconds). While this should not
    be necessary, it appears there are some network configurations where it
    is (where the agent would otherwise not reconnect after a network
    outage).

--- robot-agent/mqtt.js
+++ robot-agent/mqtt.js
@@ -48,6 +48,9 @@ const HEARTBEAT_TOPIC = '$SYS/broker/uptime';
 
 let data;
 
+// timestamp of last $SYS heartbeat from upstream
+let upstreamHeartbeat;
+
 // prefix for all our mqtt topics, i.e., our namespace
 const PREFIX = `/${process.env.TR_USERID}/${process.env.TR_DEVICEID}`;
 const version = process.env.npm_package_version;
@@ -68,13 +71,14 @@ const mqttClient = mqtt.connect(MQTT_HOST, {
   protocolVersion: 5 // needed for the `rap` option, i.e., to get retain flags
 });
 
-mqttClient.on('error', (...args) => log.warn('mqtt error', ...args));
-mqttClient.on('disconnect', (...args) => log.warn('mqtt disconnect', ...args));
+['error', 'disconnect', 'reconnect', 'offline', 'close', 'end'].forEach(event =>
+  mqttClient.on(event, (...args) => log.info(`mqtt event: ${event}`, ...args)));
 
 let initialized = false;
 let mqttSync;
 mqttClient.on('connect', function(connackPacket) {
   log.info(`${initialized ? 're-' : ''}connected to upstream mqtt broker`);
+  upstreamHeartbeat = Date.now(); // reconnects count as a sign of life from upstream
 
   if (!mqttSync) {
     mqttSync = new MqttSync({mqttClient,
@@ -157,6 +161,7 @@ mqttClient.on('connect', function(connackPacket) {
         // log.debug(`upstream mqtt, ${topic}:`, payload, packet.retain);
         // relay the upstream message to local
         if (topic == HEARTBEAT_TOPIC) {
+          upstreamHeartbeat = Date.now()
           // relay heartbeat locally:
           localBroker && localBroker.publish(packet, () => {});
         } else {
@@ -332,4 +337,28 @@ const parseLsbRelease = (string) => {
 
 const heartbeat = () => {
   data.update(`${AGENT_PREFIX}/status/heartbeat`, new Date());
+
+  // also check liveness of upstream heartbeats:
+  const timeDiff = Date.now() - upstreamHeartbeat;
+  if (timeDiff > 65 * 1000) {
+    log.warn(`No heartbeat from upstream for ${Math.round(timeDiff / 1000)} s.`);
+
+    if (global.config?.global?.aggressiveReconnect) {
+      log.warn('forcing reconnect');
+      if (mqttClient.connected) {
+        log.info('connected, end, then reconnect');
+        if (!mqttClient.reconnecting) {
+          mqttClient.end(true, () => {
+            log.info('ended, reconnect');
+            mqttClient.reconnect();
+          });
+        }
+      } else {
+        log.info('not connected, reconnect');
+        if (!mqttClient.reconnecting) {
+          mqttClient.reconnect();
+        }
+      }
+    }
+  }
 };
